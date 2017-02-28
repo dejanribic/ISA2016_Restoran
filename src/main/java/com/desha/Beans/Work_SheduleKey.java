f1110aedd1ae620d1a7360d7557f3d7b0955aeaf
ViktorBrajkovic <Viktor62@neobee.net>
2017-02-28 21:34:12 +0100

    Enjoy

---
+++ src/main/java/com/desha/Beans/Work_SheduleKey.java
@@ -0,0 +1,75 @@
+package com.desha.Beans;
+
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+/**
+ * Created by Viktor on 2/28/2017.
+ */
+@Embeddable
+public class Work_SheduleKey implements Serializable {
+
+    @Id
+    private int num;
+
+
+
+    @Id
+    @Column(name = "employee_email")
+    private String email;
+
+    @Id
+    @Column(name = "restaurant_name")
+    private String resname;
+
+    @Id
+    @Column(name = "region_id")
+    private int id;
+
+    public Work_SheduleKey(int num, String email, String resname, int id) {
+        this.num = num;
+
+        this.email = email;
+        this.resname = resname;
+        this.id = id;
+    }
+
+    public Work_SheduleKey() {
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getResname() {
+        return resname;
+    }
+
+    public void setResname(String resname) {
+        this.resname = resname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
